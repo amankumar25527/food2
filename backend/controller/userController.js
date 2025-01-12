3215adaee660f75de88013362f2fabcf5a93ae57
amankumar25527 <147196941+amankumar25527@users.noreply.github.com>
2025-01-12 23:38:31 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -75,8 +75,8 @@ const adminLogin = async (req, res) => {
     try {
       const { email, password } = req.body;
       if (
-        email === process.env.ADMIN_EMAIL &&
-        password === process.env.ADMIN_PASSWORD
+        email == process.env.ADMIN_EMAIL &&
+        password == process.env.ADMIN_PASSWORD
       ) {
         const token = jwt.sign(email + password, process.env.JWT_SECRET);
         res.json({ success: true, token });
