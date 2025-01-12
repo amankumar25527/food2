4effb97c71d49ebb74ac3e5108ea5cd94b6055d1
amankumar25527 <147196941+amankumar25527@users.noreply.github.com>
2025-01-12 23:06:14 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 // import { food_list } from "../assets/assets.js";
 export const StoreContext=createContext(null);
 const StoreContextProvider=(props)=>{
-    const url="http://localhost:4000"
+    const url="https://food-project-backend-kibv.onrender.com"
     const[cartItems,setCartItems]=useState({});
     const[token,setToken]=useState("");
     const[food_list,setFoodList]=useState([])
