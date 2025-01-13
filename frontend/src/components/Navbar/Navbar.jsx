7c9be0dbce9060664816a37d6cc56ab9d386eb7b
amankumar25527 <147196941+amankumar25527@users.noreply.github.com>
2025-01-13 12:42:42 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../../assets/assets.js'
 import { Link, useNavigate } from "react-router-dom"
 import { StoreContext } from '../../context/StoreContext.jsx'
 const Navbar = ({setShowLogin}) => {
-    const[menu,setMenu]=useState("menu");
+    const[menu,setMenu]=useState("home");
     const {getTotalCartAmount,token,setToken}=useContext(StoreContext)
     const navigate=useNavigate();
     const logout=()=>{
@@ -16,10 +16,10 @@ const Navbar = ({setShowLogin}) => {
     <div className='navbar'>
       <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar-menu">
-        <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
-        <a  href ='#footer'onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
+        <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
+        <Link to='/' href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</Link>
+        <Link to='/' href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</Link>
+        <Link to='/'  href ='#footer'onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</Link>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt=''></img>
@@ -27,7 +27,7 @@ const Navbar = ({setShowLogin}) => {
             <Link to='/cart'><img src={assets.basket_icon} alt=''></img></Link>
             <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>
+        {!token?<button onClick={()=>setShowLogin(true)}>Sign In</button>
           :<div className='navbar-profile'>
             <img src={assets.profile_icon} alt="" />
             <ul className="nav-profile-dropdown">
