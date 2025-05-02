7e4de0547856cba58be9fe916de4ccea319bf888
amankumar25527 <147196941+amankumar25527@users.noreply.github.com>
2025-05-02 10:10:38 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -59,7 +59,7 @@ const Cart = () => {
               <b>₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + delivery_fee}</b>
             </div>
           </div>
-          <button onClick={() => navigate("/order")}>PROCEED TO CHECKOUT</button>
+          <button style={{ cursor: 'pointer' }} onClick={() => navigate("/order")}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
